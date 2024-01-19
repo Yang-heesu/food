038bdd71f36ab13f1c6382a7505de852194a6d5d
Yang-heesu <hsge0602@gmail.com>
2024-01-19 11:22:08 +0900

    first commit

---
+++ src/components/layout/Menu.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+import { PAGE_URL } from "@/constants/url";
+export const Menu = () => {
+    return (
+        <div>
+            <Link href="test">test</Link>
+        </div>
+    )
+}
---
+++ src/constants/url.tsx
@@ -0,0 +1,15 @@
+export const PAGE_URL = {
+    MAIN: {
+        icon: "",
+        title: "메인화면",
+        url: ""
+    },
+    MAP: {
+        icon: "",
+        title: "지도",
+        url: ""
+    },
+    LIST: {
+
+    },
+}
